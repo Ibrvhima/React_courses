a75f6b11bbedcf11a9a27adc10fd1aa9e6bb0fb7
Ibrahima Diallo <ibrahimadialloib300@gmail.com>
2025-03-03 00:28:33 +0000

    ajout d'image des produit dans le programme

--- props/src/produits.js
+++ props/src/produits.js
@@ -1,28 +1,29 @@
 function Products(props) {
   return (
     <>
-      <p>Nom du Produit: {props.produits.nom} / {props.produits.prix} GNF</p>
+      <p>
+        Nom du Produit: {props.produits.nom} / {props.produits.prix} GNF
+      </p>
+      <img src={props.produits.image} alt={props.produits.nom} width="100" />
     </>
   );
 }
 
 export default function Fruit() {
-  const produit = [{ nom: "mango", prix: 1000 },
-     { nom: "mango", prix: 1000 },
-     { nom: "banana", prix: 1000 },
-     { nom: "apple", prix: 1000 },
-     { nom: "craps", prix: 1000 },
-     { nom: "orange", prix: 1000 }
+  const produit = [
+    { nom: "mango", prix: 1000, image: "/ai-generated-8512889_1280.jpg" },
+    { nom: "banana", prix: 1000, image: "/banana-1025109_1280.jpg" },
+    { nom: "apple", prix: 1000, image: "/apple-1834639_1280.jpg" },
+    { nom: "Grapes", prix: 1000, image: "/table-grapes-74344_1280.jpg"},
+    { nom: "orange", prix: 1000, image: "/fruits-2202426_1280.jpg" },
   ];
 
   return (
-     <div>
-          <h1>
-               liste des fruits
-          </h1>
-          {produit.map((produit,index) => (
-               <Products key={index} produits = {produit} />
-          ))}
-     </div>
-  )
+    <div>
+      <h1>liste des fruits</h1>
+      {produit.map((produit, index) => (
+        <Products key={index} produits={produit} />
+      ))}
+    </div>
+  );
 }
